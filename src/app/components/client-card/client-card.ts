c9b8b7bc4e7506386b41a8ac1b3d434cbdf7daf4
alexpetrovan2001 <alexpetrovan2001@gmail.com>
2025-11-08 11:13:10 +0200

    Work page scroll logic redesigned for tablets and phones

--- src/app/components/client-card/client-card.ts
+++ src/app/components/client-card/client-card.ts
@@ -1,17 +1,50 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, EventEmitter, Input, NgZone, OnDestroy, Output} from '@angular/core';
 import {ClientDetails} from '../../models/client-details.model';
 import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-client-card',
+  styleUrl: 'client-card.scss',
   templateUrl: './client-card.html',
   standalone: true
 })
-export class ClientCard {
+export class ClientCard implements AfterViewInit, OnDestroy {
   @Input() client!: ClientDetails;
   @Output() viewMore = new EventEmitter<void>();
 
-  constructor(private route: Router) {
+  isActive = false;
+  private observer?: IntersectionObserver;
+  mobile? = false;
+
+  constructor(private route: Router, private el: ElementRef, private zone: NgZone) {
+  }
+
+  ngAfterViewInit() {
+    this.mobile = window.innerWidth <= 1024; // only on small screens
+
+    if (!this.mobile) return; // skip IntersectionObserver on large screens
+
+    this.zone.runOutsideAngular(() => {
+      this.observer = new IntersectionObserver(
+        entries => {
+          entries.forEach(entry => {
+            this.zone.run(() => {
+              this.isActive = entry.isIntersecting && entry.intersectionRatio > 0.5;
+            });
+          });
+        },
+        {
+          root: null,
+          threshold: Array.from({length: 11}, (_, i) => i/10),
+          rootMargin: '-30% 0px -30% 0px'
+        }
+      );
+      this.observer.observe(this.el.nativeElement);
+    })
+  }
+
+  ngOnDestroy() {
+    this.observer?.disconnect();
   }
 
   onViewMore(){
